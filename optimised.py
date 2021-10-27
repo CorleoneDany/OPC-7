26bc49a04747110bbbc957e0c835474eab9acbbc
Dany Corleone <corleonedany@live.fr>
2021-10-27 18:37:48 +0200

    Modification du bruteforce pour corriger une erreur dans un for, nouvelle solution d'optimisation

--- optimised.py
+++ optimised.py
@@ -50,9 +50,67 @@ def knapSack(W, wt, val):
    
     return table[n][W] 
 
+##################### SOLUTION 3 #####################
+
+# Prints the items which are put in a
+# knapsack of capacity W
+def printknapSack(W, wt, val, n):
+    best_actions = []
+    K = [[0 for w in range(W + 1)]
+            for i in range(n + 1)]
+             
+    # Build table K[][] in bottom
+    # up manner
+    for i in range(n + 1):
+        for w in range(W + 1):
+            if i == 0 or w == 0:
+                K[i][w] = 0
+            elif wt[i - 1] <= w:
+                K[i][w] = max(val[i - 1]
+                  + K[i - 1][w - wt[i - 1]],
+                               K[i - 1][w])
+            else:
+                K[i][w] = K[i - 1][w]
+ 
+    # stores the result of Knapsack
+    res = K[n][W]
+     
+    w = W
+    for i in range(n, 0, -1):
+        if res <= 0:
+            break
+        # either the result comes from the
+        # top (K[i-1][w]) or from (val[i-1]
+        # + K[i-1] [w-wt[i-1]]) as in Knapsack
+        # table. If it comes from the latter
+        # one/ it means the item is included.
+        if res == K[i - 1][w]:
+            continue
+        else:
+ 
+            # This item is included.
+            best_actions.append(wt[i - 1])
+             
+            # Since this weight is included
+            # its value is deducted
+            res = res - val[i - 1]
+            w = w - wt[i - 1]
+    
+    print(f"Le meilleur revenu possible est de : {res}")
+    print(f"La meilleure suite d'action est la suivante : {best_actions}")
+    #print(", ".join(str(option) for option in best_actions))
+
 def knapsack_main():
-    print(solve_knapsack(return_gains_list(), return_weight_list(), 500))
-    print(knapSack(500, return_weight_list(), return_gains_list()))
+    gains_list = return_gains_list()
+    weight_list = return_weight_list()
+    length = len(gains_list)
+    #print(solve_knapsack(gains_list, weight_list, 500))
+    #print(knapSack(500, weight_list, gains_list))
+    printknapSack(500, weight_list, gains_list, length)
+
+knapsack_main()
 
 
 #Optimised = O(n)
+
+#Optimised ouvre un tableau n = capacity donc séparer capacity de money 
