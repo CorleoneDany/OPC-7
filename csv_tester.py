3fccefa23f77a0f605d8ad19776c449fed4ae021
Dany Corleone <corleonedany@live.fr>
2021-11-04 14:59:22 +0100

    Bug corrigé dans le csv tester, suppression des solutions 1 et 2 dans l'optimised

--- csv_tester.py
+++ csv_tester.py
@@ -3,23 +3,38 @@ from optimised import *
 import csv 
 
 def read_csv(csv_file):
-    weight_list = []
-    gain_list = []
-    with open(csv_file) as csv_file:
+    weights_list = []
+    gains_list = []
+    money = 500
+    with open(csv_file, mode="r", encoding="utf8") as csv_file:
         reader = csv.DictReader(csv_file, delimiter=',')
         for row in reader:
-            weight_list.append(float(row["price"]))
-            gain_list.append((float(row["price"]) * float(row["profit"])) / 100)
-        print(solve_knapsack(gain_list, weight_list, 500))
+            if float(row["profit"]) > 0 and float(row["price"]) > 0:
+                weights_list.append(round(float(row["price"]) * 100))
+                gains_list.append(round(float(row["price"]) * float(row["profit"])))
+    printknapSack(money * 100, weights_list, gains_list)
+
 
         #Le problème est que la capacité est parfois en float mais elle est utilisée 
         #en tant qu'index dans le tableau des 2 methodes knapsack
 
         #Multiplier toutes les valeurs par 100 puis les diviser ensuite par 100 ?
+        #Multiplier les valeurs par 100 ferait un algo bien plus lourd car le tableau
+        #dynamique créé est plus gros lorsque la capacité est plus grosse donc dans ce cas
+        #100 fois plus grosse
 
         #Arrondir l'argent restant au plus bas ? plus haut ? 
         #Ok si les résultats ne sont pas faussés a cause des centimes ce qui me semble
         #peu probable
+
+        #Relire le fonctionnement de l'algo knapskack et le comprendre a 100%
+        #index arrondi ou ifs ? corriger intelligement le soucis d'index
+
+        #Se renseigner sur le discord P7, la solution y est peut être déjà
+        #Corentin sur le P7
+
+        #Separer la valeur argent et la valeur de taille de tableau
+        #Ne pas oublier qu'une variable ne doit avoir qu'une seule utilité si possible 
 
 read_csv("dataset1.csv")
 read_csv("dataset2.csv")
--- optimised.py
+++ optimised.py
@@ -1,63 +1,18 @@
 from bruteforce import *
 
-##################### SOLUTION 1 #####################
-
-def solve_knapsack(profits, weights, capacity):
-    # create a two dimensional array for Memoization, each element is initialized to '-1'
-    dp = [[-1 for x in range(capacity+1)] for y in range(len(profits))]
-    return knapsack_recursive(dp, profits, weights, capacity, 0)
-
-
-def knapsack_recursive(dp, profits, weights, capacity, currentIndex):
-
-    # base case checks
-    if capacity <= 0 or currentIndex >= len(profits):
-        return 0
-
-    # if we have already solved a similar problem, return the result from memory
-    if dp[currentIndex][capacity] != -1:
-        return dp[currentIndex][capacity]
-
-    # recursive call after choosing the element at the currentIndex
-    # if the weight of the element at currentIndex exceeds the capacity, we
-    # shouldn't process this
-    profit1 = 0
-    if weights[currentIndex] <= capacity:
-        profit1 = profits[currentIndex] + knapsack_recursive(
-        dp, profits, weights, capacity - weights[currentIndex], currentIndex + 1)
-
-    # recursive call after excluding the element at the currentIndex
-    profit2 = knapsack_recursive(
-    dp, profits, weights, capacity, currentIndex + 1)
-
-    dp[currentIndex][capacity] = max(profit1, profit2)
-    return dp[currentIndex][capacity]
-
-##################### SOLUTION 2 #####################
-
-def knapSack(W, wt, val): 
-    n = len(val)
-    table = [[0 for x in range(W + 1)] for x in range(n + 1)] 
- 
-    for i in range(n + 1): 
-        for j in range(W + 1): 
-            if i == 0 or j == 0: 
-                table[i][j] = 0
-            elif wt[i-1] <= j: 
-                table[i][j] = max(val[i-1] + table[i-1][j-wt[i-1]],  table[i-1][j]) 
-            else: 
-                table[i][j] = table[i-1][j] 
-   
-    return table[n][W] 
-
-##################### SOLUTION 3 #####################
-
 # Python3 code for Dynamic Programming
 # based solution for 0-1 Knapsack problem
 
 # Prints the items which are put in a
 # knapsack of capacity W
-def printknapSack(W, wt, val, n):
+def printknapSack(W, wt, val):
+    if len(wt) != len(val):
+        print("Wt and val are different length!")
+    elif len(wt) == len(val):
+        print("Wt and val are same length!")
+
+
+    n = len(val)
     best_actions = []
     K = [[0 for w in range(W + 1)]
             for i in range(n + 1)]
@@ -77,7 +32,7 @@ def printknapSack(W, wt, val, n):
 
     # stores the result of Knapsack
     res = K[n][W]
-    print(f"Le meilleur revenu possible est de : {res}")
+    print(f"Le meilleur revenu possible est de : {float(res) / 100}")
     
     w = W
     for i in range(n, 0, -1):
@@ -102,17 +57,6 @@ def printknapSack(W, wt, val, n):
     
     print(f"La meilleure suite d'action est la suivante : {best_actions}")
     #print(", ".join(str(option) for option in best_actions))
-
-
-def knapsack_main():
-    gains_list = return_gains_list()
-    weight_list = return_weight_list()
-    length = len(gains_list)
-    #print(solve_knapsack(gains_list, weight_list, 500))
-    #print(knapSack(500, weight_list, gains_list))
-    printknapSack(500, weight_list, gains_list, length)
-
-knapsack_main()
 
 
 #Optimised = O(n)
